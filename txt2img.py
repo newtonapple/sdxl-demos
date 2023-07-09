dadeaa845d62c0d6b0b0777dd2dd2e478731ee69
David Dai <david.github@gmail.com>
2023-07-09 16:53:29 -0700

    Cleanup txt2img.py: remove argv printouts.

--- txt2img.py
+++ txt2img.py
@@ -1,11 +1,10 @@
 import utils
 import sys
 
-print(sys.argv)
+if __name__ == "__main__":
+    prompt = sys.argv[1]
+    outfile = sys.argv[2]
+    pipe = utils.pipeline()
 
-prompt = sys.argv[1]
-outfile = sys.argv[2]
-pipe = utils.pipeline()
-
-image = pipe(prompt=prompt).images[0]
-image.save(outfile)
+    image = pipe(prompt=prompt).images[0]
+    image.save(outfile)
