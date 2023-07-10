ae46797f6379fb43875917b435c718a228a88d45
David Dai <david.github@gmail.com>
2023-07-10 02:00:20 -0700

    Add seed to WebUI.

--- txt2img_webui.py
+++ txt2img_webui.py
@@ -1,4 +1,6 @@
 import utils
+import torch
+import random
 import gradio as gr
 
 
@@ -9,6 +11,7 @@ class WebUI:
         inputs = [
             gr.Textbox(),  # prompt
             gr.Textbox(),  # negative prompt
+            gr.Textbox(value=-1),  # seed
             gr.Slider(5, 2000, value=1024, step=8),  # width
             gr.Slider(5, 2000, value=1024, step=8),  # height
             gr.Slider(0, 200, value=50, step=1),  # steps
@@ -21,23 +24,40 @@ class WebUI:
         self,
         prompt,
         negative_prompt,
+        seed,
         width,
         height,
         steps,
         guidance_scale,
         num_images_per_prompt,
     ):
+        seed = int(seed)
+        if seed <= -1:
+            seeds = [
+                random.randint(1, 99999999999) for _ in range(0, num_images_per_prompt)
+            ]
+            generator = [
+                torch.Generator(device=self.pipeline.device).manual_seed(s)
+                for s in seeds
+            ]
+        else:
+            seeds = [seed + i for i in range(0, num_images_per_prompt)]
+            generator = [
+                torch.Generator(device=self.pipeline.device).manual_seed(s)
+                for s in seeds
+            ]
+
         output = self.pipeline(
             prompt=prompt,
             negative_prompt=negative_prompt,
+            generator=generator,
             width=width,
             height=height,
             num_inference_steps=steps,
             guidance_scale=guidance_scale,
             num_images_per_prompt=num_images_per_prompt,
         )
-        # output = self.pipeline(prompt=prompt)
-        return output.images
+        return [[i[1], seeds[i[0]]] for i in enumerate(output.images)]
 
     def launch(self, *args, **kwargs):
         self.webui.launch(*args, **kwargs)
